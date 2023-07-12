fa23c24caed9152cda6515bb3449b8cd85b71e3d
qcloud <ubuntu@localhost.localdomain>
2023-07-12 14:40:57 +0800

    init proj

---
+++ main.js
@@ -0,0 +1,9 @@
+const Koa = require('koa')
+const serve = require('koa-static')
+const app = new Koa()
+app.use(serve('~/www/front-end/koaapp-frontend/dist'))
+// app.use(async ctx => {
+//	ctx.body = 'hello koa'
+//})
+
+app.listen(3000)
